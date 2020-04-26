de4fe45614e25e1ef85378e62464e92229554b77
Garrett <LuckyGY98@gmail.com>
2020-04-26 11:42:16 -0700

    Completed part 1

--- turnin/gyama009_lab5_part1.c
+++ turnin/gyama009_lab5_part1.c
@@ -16,9 +16,14 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs
 	unsigned char temp = 0x00; // Temporary variable to hold PINA
+	unsigned char hold = 0x00; // Temporary variable to hold temp
 	while (1) {
 		temp = ~PINA;
-		PORTC = ((temp >= 1) << 5) + ((temp >= 3) << 4) + ((temp >= 5) << 3) + ((temp >= 7) << 2) + ((temp >= 10) << 1) + ((temp >= 13) << 0) + ((temp <= 4) << 6);
+		hold = ((temp >= 1) << 5) + ((temp >= 3) << 4) + ((temp >= 5) << 3) + ((temp >= 7) << 2) + ((temp >= 10) << 1) + ((temp >= 13) << 0) + ((temp <= 4) << 6);
+		if(temp == 0x83){
+			hold = 0x70;
+		}
+		PORTC = hold;
 	}
 	return 0;
 }
