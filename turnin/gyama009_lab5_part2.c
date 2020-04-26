7aace573a11b3aeff8714450be9b1666a4ba9aba
Garrett <LuckyGY98@gmail.com>
2020-04-26 15:09:02 -0700

    Completed part 2

--- turnin/gyama009_lab5_part2.c
+++ turnin/gyama009_lab5_part2.c
@@ -1,24 +1,104 @@
-/*	Author: gyama009
+/*	Author: Garrett Yamano
  *  Partner(s) Name: 
  *	Lab Section: 022
- *	Assignment: Lab #3 Exercise #1
+ *	Assignment: Lab #5 Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
+
+// Demo: link_to_video.com
+
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+enum States{Start, Init, Increment, Decrement, Reset} state;
+
+void Tick(){
+	switch(state){
+		case Start: // Initial transition
+			PORTC = 0x00;
+			state = Init;
+			break;
+		case Init:
+			if((~PINA & 0xFF) == 0x01){
+				state = Increment;
+				if(PORTC <= 0x08){
+					PORTC += 0x01;
+				}
+			}
+			else if(((~PINA & 0xFF) == 0x02)){
+				state = Decrement;
+				if(PORTC >= 0x01){
+					PORTC -= 0x01;
+				}
+			}
+			else if(((~PINA & 0xFF) == 0x03)){
+				state = Reset;
+			}
+			else{
+				state = Init;
+			}
+			break;
+		case Increment:
+			if((~PINA & 0xFF) == 0x00){
+				state = Init;
+			}
+			else if((~PINA & 0xFF) == 0x03){
+				state = Reset;
+			}
+			else{
+				state = Increment;
+			}
+			break;
+		case Decrement:
+			if((~PINA & 0xFF) == 0x00){
+				state = Init;
+			}
+			else if((~PINA & 0xFF) == 0x03){
+				state = Reset;
+			}
+			else{
+				state = Decrement;
+			}
+			break;
+		case Reset:
+			if(((~PINA & 0xFF) == 0x03) || ((~PINA & 0xFF) == 0x02) ||((~PINA & 0xFF) == 0x01)){
+				state = Reset;
+			}
+			else{
+				state = Init;
+			}
+			break;
+		default:
+			state = Start;
+			break;
+	} // Transitions
+	switch(state){ // State actions
+		case Init:
+			break;
+		case Increment:
+			break;
+		case Decrement:
+			break;
+		case Reset:
+			PORTC = 0x00;
+			break;
+		default:
+			break;
+	} // State actions
+}
+
 int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs
-	unsigned char temp = 0x00; // Temporary variable to hold PINA
+	state = Start;
 	while (1) {
-		temp = ~PINA;
-		PORTC = ((temp >= 1) << 5) + ((temp >= 3) << 4) + ((temp >= 5) << 3) + ((temp >= 7) << 2) + ((temp >= 10) << 1) + ((temp >= 13) << 0) + ((temp <= 4) << 6);
+		Tick();	
 	}
 	return 0;
 }
+
